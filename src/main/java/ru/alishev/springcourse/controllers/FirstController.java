751f34c852cdd9640194dd56301f627a2d8c85df
AlexandrKozlov1980 <A362132@yandex.ru>
2021-09-06 21:43:08 +0300

    changes in classes

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,20 +1,37 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+//    public String helloPage(HttpServletRequest request){
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//
+//        System.out.println("Hello " + name +" " +surname);
+//        return "first/hello";
+//    }
+
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname, Model model){
+
+        System.out.println("Hello " + name +" " +surname);
+        model.addAttribute("message", "Hello " + name +" " +surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage(){
-         return "first/goodbye";
+
+        return "first/goodbye";
     }
 }
